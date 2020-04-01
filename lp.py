e7a2f4f26afb4ece1c20bb67604c2980faefe4cc
Joe Guo <joe.guo@canonical.com>
2020-04-01 13:16:09 +1300

    lp.py: move more functions to parser
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- lp.py
+++ lp.py
@@ -26,19 +26,46 @@ else:
         pass
 
 
-class WeeChatArgParser(argparse.ArgumentParser):
+class WeeChatArgumentParser(argparse.ArgumentParser):
 
     def _print_message(self, message, file=None):
+        """print message to core buffer.
+
+        Help to avoid exception:
+
+            AttributeError: module 'weechatOutputs' has no attribute 'flush'
+
+        """
         if message:
             log(message)
 
     def exit(self, status=0, message=None):
+        """convert exit code"""
         if message:
             self._print_message(message)
         if status == 0:
-            sys.exit(weechat.WEECHAT_RC_OK)
+            code = weechat.WEECHAT_RC_OK
         else:
-            sys.exit(weechat.WEECHAT_RC_ERROR)
+            code = weechat.WEECHAT_RC_ERROR
+        sys.exit(code)
+
+    def parse_args(self, args=None, namespace=None):
+        """convert str args to argv list"""
+        if args and isinstance(args, str):
+            # --option "I have space" --> ["option", "I have space"]
+            args = shlex.split(args)
+        return super().parse_args(args=args, namespace=namespace)
+
+    def hook_command(self, hook_func_name):
+        weechat.hook_command(
+            NAME,
+            self.description,
+            '',  # usage, included in help, no need to repeat
+            self.format_help(),
+            '',  # complete template
+            hook_func_name,
+            '',
+        )
 
 
 def set_option(option, value, save=True):
@@ -58,7 +85,7 @@ def get_option_str(option):
 
 
 def build_parser():
-    parser = WeeChatArgParser(
+    parser = WeeChatArgumentParser(
         prog=NAME,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description=DESC,
@@ -73,16 +100,7 @@ def build_parser():
 
 
 parser = build_parser()
-
-weechat.hook_command(
-    NAME,
-    parser.description,
-    '',  # usage, included in help
-    parser.format_help(),
-    '',  # complete template
-    'hook_command',
-    '',
-)
+parser.hook_command('hook_command')
 
 
 def hook_command(data, buffer, args):
@@ -97,8 +115,6 @@ def hook_command(data, buffer, args):
 
 
 def main(data, buffer, args):
-    # --option "I have space" --> ["option", "I have space"]
-    args = shlex.split(args)
     cli = parser.parse_args(args=args)
     for id_ in cli.ids:
         url = 'https://launchpad.net/bugs/{}'.format(id_)
