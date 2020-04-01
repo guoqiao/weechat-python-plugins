8148745df92bd3028fdc17f540c11a6da71a115a
Joe Guo <joe.guo@canonical.com>
2020-04-01 13:40:03 +1300

    lp.py: move more to WeeChatPlugin
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- lp.py
+++ lp.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import io
 import sys
 import shlex
 import argparse
@@ -7,25 +6,26 @@ import weechat
 
 NAME = 'lp'
 DESC = 'print url for launchpad bug'
+AUTHOR = 'guoqiao'
+VERSION = '20200401'
+LICENCE = 'MIT'
 
 VERBOSE = False
 
-weechat.register(NAME, "guoqiao", "20200401", "MIT", DESC, "", "")
 
+class WeeChatPlugin(argparse.ArgumentParser):
 
-def log(message, buffer=''):
-    weechat.prnt(buffer, '{}'.format(message))
+    def __init__(self, *args, **kwargs):
+        weechat.register(NAME, AUTHOR, VERSION, LICENCE, DESC, "", "")
+        super().__init__(*args, **kwargs)
 
+    def prnt(self, message, buffer=''):
+        if message:
+            weechat.prnt(buffer, '{}'.format(message))
 
-if VERBOSE:
-    def verbose(message):
-        log(message)
-else:
-    def verbose(message):
-        pass
-
-
-class WeeChatArgumentParser(argparse.ArgumentParser):
+    def verbose(self, message):
+        if VERBOSE:
+            self.prnt(message)
 
     def _print_message(self, message, file=None):
         """print message to core buffer.
@@ -35,8 +35,7 @@ class WeeChatArgumentParser(argparse.ArgumentParser):
             AttributeError: module 'weechatOutputs' has no attribute 'flush'
 
         """
-        if message:
-            log(message)
+        self.prnt(message)
 
     def exit(self, status=0, message=None):
         """convert exit code"""
@@ -66,56 +65,44 @@ class WeeChatArgumentParser(argparse.ArgumentParser):
             '',
         )
 
+    def set_option(self, option, value, save=True):
+        cmd = '/set {} {}'.format(option, value)
+        self.verbose(cmd)
+        weechat.command('', cmd)
+        if save:
+            weechat.command('', '/save')
 
-def set_option(option, value, save=True):
-    cmd = '/set {} {}'.format(option, value)
-    verbose(cmd)
-    weechat.command('', cmd)
-    if save:
-        weechat.command('', '/save')
+    def get_option(self, option):
+        return weechat.config_get(option)
 
-
-def get_option(option):
-    return weechat.config_get(option)
-
-
-def get_option_str(option):
-    return weechat.config_string(get_option(option))
+    def get_option_str(self, option):
+        return weechat.config_string(self.get_option(option))
 
 
-def build_parser():
-    parser = WeeChatArgumentParser(
-        prog=NAME,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description=DESC,
-    )
+parser = WeeChatPlugin(
+    prog=NAME,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    description=DESC,
+)
 
-    parser.add_argument(
-        'ids', metavar='LP_BUG_ID', nargs='+',
-        help='launchpad bug id, can repeat',
-    )
+parser.add_argument(
+    'ids', metavar='LP_BUG_ID', nargs='+',
+    help='launchpad bug id, can repeat',
+)
 
-    return parser
-
-
-parser = build_parser()
-parser.hook_command('hook_command')
-
-
-def hook_command(data, buffer, args):
-    try:
-        return main(data, buffer, args)
-    except SystemExit as exc:
-        # catch sys.exit and return proper code for weechat
-        return exc.code
-    except Exception as exc:
-        log(exc)
-        return weechat.WEECHAT_RC_ERROR
+parser.hook_command('main')
 
 
 def main(data, buffer, args):
-    cli = parser.parse_args(args=args)
-    for id_ in cli.ids:
-        url = 'https://launchpad.net/bugs/{}'.format(id_)
-        log(url, buffer=buffer)
-    return weechat.WEECHAT_RC_OK
+    try:
+        cli = parser.parse_args(args=args)
+        for id_ in cli.ids:
+            url = 'https://launchpad.net/bugs/{}'.format(id_)
+            parser.prnt(url, buffer=buffer)
+        return weechat.WEECHAT_RC_OK
+    except SystemExit as exc:
+        # catch sys.exit from parse_args and return proper code for weechat
+        return exc.code
+    except Exception as exc:
+        parser.prnt(exc)
+        return weechat.WEECHAT_RC_ERROR
