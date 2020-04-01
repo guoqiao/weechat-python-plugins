e00877fd0f41b5d7f21a9645208d43d8abd08003
Joe Guo <joe.guo@canonical.com>
2020-04-02 02:52:38 +1300

    weechat_plugin.py: rm server which is not generic
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- weechat_plugin.py
+++ weechat_plugin.py
@@ -67,7 +67,6 @@ class WeeChatPlugin(argparse.ArgumentParser):
             args = shlex.split(args)
         cli = super().parse_args(args=args, namespace=namespace)
         self._verbose = cli.verbose
-        self.server = cli.server
         return cli
 
     def hook_command(self, hook_func_name):
