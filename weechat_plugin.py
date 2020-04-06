797eb6905ee6cdae6dc360e71f1a94e59890b218
Joe Guo <joe.guo@canonical.com>
2020-04-06 12:18:58 +1200

    weechat_plugin.py: print buffer with verbose
    Signed-off-by: Joe Guo <joe.guo@canonical.com>

--- weechat_plugin.py
+++ weechat_plugin.py
@@ -61,12 +61,13 @@ class WeeChatPlugin(argparse.ArgumentParser):
 
     def parse_args(self, args=None, namespace=None, buffer=''):
         """convert str args to argv list"""
-        self.buffer = buffer
         if args and isinstance(args, str):
             # --option "I have space" --> ["option", "I have space"]
             args = shlex.split(args)
         cli = super().parse_args(args=args, namespace=namespace)
         self._verbose = cli.verbose
+        self.buffer = buffer
+        self.verbose('buffer: {}'.format(buffer))
         return cli
 
     def hook_command(self, hook_func_name):
